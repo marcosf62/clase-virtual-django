adbf7c5f2c4d3eb103c414489137241bd2287c4e
marcosf62 <m.funes@itecriocuarto.org.ar>
2026-04-06 09:11:55 -0300

    agrego mascotas

--- saludo/views.py
+++ saludo/views.py
@@ -2,14 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+contexto = {
+        "nombre":"juancito",
+        "esMayor": True,
+        "mascotas": ["Firulais", "Bobby", "Bravo"] 
+        }
+
+
 def saludo(request):
-
-    contexto = {"nombre":"juancito"}
-
     return render(request,'saludo/index.html', contexto)
 
 def despedir(request):
-    return HttpResponse("<h1>adios Mundo</h1>")
+    return render(request,'saludo/despedir.html')
 
 def inicio(request):
     return HttpResponse("<h1>estoy en root</h1>")
